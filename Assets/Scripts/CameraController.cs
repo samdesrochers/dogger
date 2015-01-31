b5622c45309ccc73cbb35bdc93592e583a929195
samdesrochers <samdesrochers@gmail.com>
2015-01-31 11:56:06 -0800

    Camera following player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	private Transform playerRef;
+
+	void Start () {
+		playerRef = GameObject.FindWithTag ("Player").transform;
+	}
+	
+	void Update () {
+	
+		// Adjust camera on player's position
+		Camera.main.transform.position = new Vector3(playerRef.position.x, 
+		                                             playerRef.position.y, 
+		                                             Camera.main.transform.position.z);
+	}
+}
