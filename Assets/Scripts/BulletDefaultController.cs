178d94a7f050451eeeca68a435f74304b59728d3
Jenkins <Jenkins>
2015-01-31 15:01:43 -0800

    Click to shoot gg

---
+++ Assets/Scripts/BulletDefaultController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDefaultController : MonoBehaviour {
+
+	// Time before the bullet disappears, in seconds
+	public float TimeToLive = 5f;
+
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(this.gameObject, this.TimeToLive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
