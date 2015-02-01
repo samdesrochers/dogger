d8a730ddfed40346aff8d38c7a3f1816896e57a9
Jenkins <Jenkins>
2015-01-31 19:20:34 -0800

    Ajouté sélection de weapon, appuyer sur 1-2-3-4

---
+++ Assets/GameData/Weapons/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon {
+
+	public Object ProjectilePrefab;
+	public float ProjectileForce;
+	public float Cooldown;	// Weapon cooldown, in seconds
+
+	public Weapon(string projPrefabPath, float projForce, float cooldown) {
+		this.ProjectilePrefab = Resources.Load(projPrefabPath);
+		this.ProjectileForce = projForce;
+		this.Cooldown = cooldown;
+	}
+}
--- Assets/Scripts/WeaponClickController.cs
+++ Assets/Scripts/WeaponClickController.cs
@@ -1,20 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class WeaponClickController : MonoBehaviour {
-
-	public Rigidbody2D ProjectilePrefab;
+	
 	public float ProjectileForceModifier;
+
+	private List<Weapon> weapons;
+	private int currentWeapon;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.weapons = new List<Weapon>();
+
+		Weapon pistol = new Weapon("Prefabs/Projectiles/pistol", 15f, 0.7f);
+		Weapon smg = new Weapon("Prefabs/Projectiles/machine_gun", 20f, 0.2f);
+		Weapon laserRifle = new Weapon("Prefabs/Projectiles/laser_red", 30f, 1f);
+		Weapon photonRifle = new Weapon("Prefabs/Projectiles/photon", 5f, 1.5f);
+
+		this.weapons.Add(pistol);
+		this.weapons.Add(smg);
+		this.weapons.Add(laserRifle);
+		this.weapons.Add(photonRifle);
+
+		this.currentWeapon = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
 			this.FireBullet();
+		}
+
+		if (Input.GetKey(KeyCode.Alpha1)) {
+			this.currentWeapon = 0;
+		}
+
+		if (Input.GetKey(KeyCode.Alpha2)) {
+			this.currentWeapon = 1;
+		}
+
+		if (Input.GetKey(KeyCode.Alpha3)) {
+			this.currentWeapon = 2;
+		}
+
+		if (Input.GetKey(KeyCode.Alpha4)) {
+			this.currentWeapon = 3;
 		}
 	}
 
@@ -27,8 +57,10 @@ public class WeaponClickController : MonoBehaviour {
 		direction.Normalize();
 		
 		float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		
-		Rigidbody2D bulletInstance = (Rigidbody2D)Instantiate(this.ProjectilePrefab, this.transform.position, Quaternion.Euler(0, 0, targetAngle));
-		bulletInstance.AddForce(direction * this.ProjectileForceModifier);
+		Weapon weapon = this.weapons[this.currentWeapon];
+
+		GameObject bulletInstance = (GameObject)Instantiate(weapon.ProjectilePrefab, this.transform.position, Quaternion.Euler(0, 0, targetAngle));
+		Vector2 force = direction * weapon.ProjectileForce * this.ProjectileForceModifier;
+		bulletInstance.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
 	}
 }
