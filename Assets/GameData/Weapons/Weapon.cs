fdef4d83ffed99b571171d68edb660de7e73084d
Jenkins <jenkins@example.com>
2015-02-08 14:10:02 -0800

    Added a gun in the player's hand; projectiles now fire from the gun; the gun aims at the cursor when the cursor is far enough from the player; pistol no longer has a cooldown but is no longer automatic.

--- Assets/GameData/Weapons/Weapon.cs
+++ Assets/GameData/Weapons/Weapon.cs
@@ -2,33 +2,43 @@
 using System.Collections;
 
 public class Weapon {
-
+	
 	private Object projectilePrefab;
 	private float projectileForce;
 	private float cooldown;	// Weapon cooldown, in seconds
 	private float lastFired;
-
+	private bool isAuto;
+	private bool isTriggerDown;
+	
 	public Weapon(string projPrefabPath, float projForce, float cooldown) {
 		this.projectilePrefab = Resources.Load(projPrefabPath);
 		this.projectileForce = projForce;
 		this.cooldown = cooldown;
+		this.lastFired = 0.0f;
+		this.isAuto = cooldown == 0.0f ? false : true;
+		this.isTriggerDown = false;
 	}
-
+	
 	public bool CanFire() {
-		return Time.time > this.lastFired + this.cooldown;
+		if (this.isAuto) {
+			return Time.time > this.lastFired + this.cooldown;
+		} else {
+			return !this.isTriggerDown;
+		}
 	}
-
-	public void Fire(Vector3 position, Vector3 target, float forceModifier) {
-		Vector3 direction = target - position;
+	
+	public void Fire(Transform weaponTransform, Vector3 target) {
+		Vector3 direction = weaponTransform.rotation * Vector3.left;
+		Vector2 force = direction * this.projectileForce;
 		
-		direction.z = 0;
-		direction.Normalize();
-		
-		float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;		
-		GameObject bulletInstance = (GameObject)MonoBehaviour.Instantiate(this.projectilePrefab, position, Quaternion.Euler(0, 0, targetAngle));
-		Vector2 force = direction * this.projectileForce * forceModifier;
-
+		GameObject bulletInstance = (GameObject)MonoBehaviour.Instantiate(this.projectilePrefab, weaponTransform.position, weaponTransform.rotation);
 		bulletInstance.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+		bulletInstance.GetComponent<SpriteRenderer>().sortingLayerName = "ProjectilesLayer";
 		this.lastFired = Time.time;
+		this.isTriggerDown = true;
+	}
+	
+	public void ReleaseTrigger() {
+		this.isTriggerDown = false;
 	}
 }
--- Assets/Scripts/WeaponClickController.cs
+++ Assets/Scripts/WeaponClickController.cs
@@ -2,52 +2,72 @@
 using System.Collections.Generic;
 
 public class WeaponClickController : MonoBehaviour {
+	public float minAimDistance = 1.4f;
 	
-	public float ProjectileForceModifier;
-
 	private List<Weapon> weapons;
 	private int currentWeapon;
-
+	
 	// Use this for initialization
 	void Start () {
 		this.weapons = new List<Weapon>();
-
-		Weapon pistol = new Weapon("Prefabs/Projectiles/pistol", 20f, 0.6f);
-		Weapon smg = new Weapon("Prefabs/Projectiles/machine_gun", 30f, 0.15f);
-		Weapon laserRifle = new Weapon("Prefabs/Projectiles/laser_red", 50f, 0.85f);
-		Weapon photonRifle = new Weapon("Prefabs/Projectiles/photon", 5f, 1.15f);
-
+		
+		Weapon pistol = new Weapon("Prefabs/Projectiles/pistol", 20f, 0.0f);
+		Weapon smg = new Weapon("Prefabs/Projectiles/machine_gun", 30f, 0.1f);
+		Weapon laserRifle = new Weapon("Prefabs/Projectiles/laser_red", 35f, 0.5f);
+		Weapon photonRifle = new Weapon("Prefabs/Projectiles/photon", 5f, 0.75f);
+		
 		this.weapons.Add(pistol);
 		this.weapons.Add(smg);
 		this.weapons.Add(laserRifle);
 		this.weapons.Add(photonRifle);
-
+		
 		this.currentWeapon = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Transform leftGun = this.transform.Find("LeftGunContainer");
+		Vector3 mouseTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mouseTarget.z = 0f;
+		
+		// Orient weapons towards cursor
+		float aimDistance = Vector3.Distance(leftGun.position, mouseTarget);
+		if (aimDistance > this.minAimDistance) {
+			Vector3 direction = mouseTarget - leftGun.position;		
+			direction.Normalize ();
+			
+			float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			leftGun.transform.rotation = Quaternion.Euler(0f, 0f, targetAngle - 180.0f);
+		}
+		
+		// Fire weapon
 		if (Input.GetButton("Fire1")) {
 			Weapon weapon = this.weapons[this.currentWeapon];
 			if (weapon.CanFire()) {
-				Vector3 position = this.transform.position;
-				Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				weapon.Fire(position, target, this.ProjectileForceModifier);
+				weapon.Fire(leftGun, mouseTarget);
 			}
 		}
-
+		
+		// Release trigger
+		if (Input.GetButtonUp ("Fire1")) {
+			foreach(Weapon w in this.weapons) {
+				w.ReleaseTrigger();
+			}
+		}
+		
+		// Change weapon
 		if (Input.GetKey(KeyCode.Alpha1)) {
 			this.currentWeapon = 0;
 		}
-
+		
 		if (Input.GetKey(KeyCode.Alpha2)) {
 			this.currentWeapon = 1;
 		}
-
+		
 		if (Input.GetKey(KeyCode.Alpha3)) {
 			this.currentWeapon = 2;
 		}
-
+		
 		if (Input.GetKey(KeyCode.Alpha4)) {
 			this.currentWeapon = 3;
 		}
