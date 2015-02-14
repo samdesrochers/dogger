cc0a322c2fae7f3369fac0b322ea4b8d66909dcd
Kevin Bruneau <kbruneau@beehivr.com>
2015-02-14 15:34:41 -0500

    Bloodsplash is in the right direction

--- Assets/Scripts/ProjectileDefaultController.cs
+++ Assets/Scripts/ProjectileDefaultController.cs
@@ -6,16 +6,17 @@ public class ProjectileDefaultController : MonoBehaviour {
 	// Time before the bullet disappears, in seconds
 	public float TimeToLive = 5f;
 
-	private Vector3 direction;
+	private Vector2 direction;
 
 	// Use this for initialization
 	void Start () {
 		Destroy(this.gameObject, this.TimeToLive);
+		
+		direction = this.rigidbody2D.velocity;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 	}
 
 	void OnCollisionEnter2D(Collision2D coll
@@ -23,17 +24,17 @@ public class ProjectileDefaultController : MonoBehaviour {
 		// To change keyvohn
 		if (coll.gameObject.tag != "Player") {
 			// Blood splash
-			Object projectilePrefab = Resources.Load("Prefabs/Projectiles/bloodSplash");;
-			GameObject e = (GameObject)Instantiate(projectilePrefab, coll.gameObject.transform.position, Quaternion.Euler(0, 0, 0));
+			float tireAngle = Mathf.Rad2Deg*Mathf.Atan2(-direction.y,direction.x);
 
-			ParticleSystem pogo = e.GetComponent<ParticleSystem>();
-			pogo.gameObject.SetActive(true);
-			pogo.enableEmission = true;
-			pogo.Play();
+			GameObject e = (GameObject)Instantiate(Resources.Load("Prefabs/Projectiles/bloodSplash"), coll.gameObject.transform.position, Quaternion.Euler(tireAngle,90,0)); 
+			ParticleSystem spashParticleSystem = e.GetComponent<ParticleSystem>();
+			spashParticleSystem.gameObject.SetActive(true);
+			spashParticleSystem.enableEmission = true;
+			spashParticleSystem.Play();
+			Destroy(spashParticleSystem.gameObject, 0.1f);
 
-			Debug.Log("Dam son dat bullet hit !!!");
 			coll.gameObject.SendMessage("TakeDamage", 10, SendMessageOptions.DontRequireReceiver);
-//			Destroy (this.gameObject);	
+			Destroy (this.gameObject);	
 		}
 	}
 }
