5710a4e79f1673dc1a11662bacee12a776d0c1bd
samdesrochers <samdesrochers@gmail.com>
2015-02-15 14:25:36 -0800

    fix

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+	public GameManager GameManager;
+
+	// Hero Attributes
+	public Text[] PlayerStats;
+	public Text[] Debug;
+
+	UIHelper ui;
+
+	void Start()
+	{
+		Initialize();
+	}
+	
+	private void Initialize()
+	{
+
+	}
+	
+	void Update()
+	{
+		ui = GameManager.GetUIFrame ();
+
+		PlayerStats [0].text = ui.HealthCurrent + "/" + ui.HealthTotal;
+		Debug [0].text = ui.posX + ", " + ui.posY;
+
+	}
+}
---
+++ Assets/Scripts/Util/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	
+	public AudioSource SongSource; 
+	public AudioSource SoundSource;
+	
+	public AudioClip[] SongClips;
+	public AudioClip[] SoundClips;
+	
+	// For reference on how to load a clip programmaticaly
+	private AudioClip loadedClip;
+	
+	public enum Sounds
+	{
+		lolz,
+		thunder
+	};
+	
+	private int songIndex = 0;
+	
+	void Start () 
+	{
+		SongSource.volume = 0.7f;
+		SoundSource.volume = 0.7f;
+		
+		loadedClip = (AudioClip) Resources.Load ("Audio/Sounds/sound-thunder");
+	}
+	
+	void Update()
+	{
+		if(Input.GetKey("m")) {
+			PlayNextSong();
+		}
+		
+		if(Input.GetKey("l")) {
+			PlaySound((int)Sounds.lolz);
+		}
+		
+		if(Input.GetKey("t")) {
+			PlaySound((int)Sounds.thunder);
+		}
+		
+		if(Input.GetKey("c")) {
+			PlaySound(loadedClip);
+		}
+	}
+	
+	void PlayNextSong() 
+	{
+		SongSource.clip = SongClips[songIndex];
+		SongSource.Play();
+		
+		songIndex = (songIndex + 1) % SongClips.Length;
+	}
+	
+	/// <summary>
+	/// Plays a pre loaded sound found in the Sound enum - Index pairing.
+	/// </summary>
+	/// <param name="index">Index.</param>
+	void PlaySound(int index)
+	{
+		SoundSource.clip = SoundClips[index];
+		SoundSource.Play();
+	}
+	
+	/// <summary>
+	/// Plays a sound from an existing AudioClip
+	/// </summary>
+	/// <param name="clip">Clip.</param>
+	void PlaySound(AudioClip clip)
+	{
+		SoundSource.clip = clip;
+		SoundSource.Play();
+	}
+}
